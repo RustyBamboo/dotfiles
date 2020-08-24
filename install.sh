f6fcf84ac8914a5fee62e9984bacd9b8ef290249
RustyBamboo <dvolya@gmail.com>
2020-08-24 10:11:09 -0400

    Add wob, swappy, and seat configuration for wacom tablet
    - Uses new -S seat selection in swayidle

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -s $PWD/mako $HOME/.config/
 ln -s $PWD/sway $HOME/.config/
 ln -s $PWD/waybar $HOME/.config/
 ln -s $PWD/wofi $HOME/.config/
+ln -s $PWD/wlogout $HOME/.config/
+
+# Building sway from source
+ln -s $PWD/environment.d $HOME/.config/
+ln -s $PWD/local-lib.conf /etc/ld.so.conf.d/local-lib.conf
